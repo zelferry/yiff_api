0f79a0acdb20559dfea4605cece0778eb86890db
Zelferry <>
2021-05-15 18:33:08 +0000

    AASSA

--- src/upgrades.js
+++ src/upgrades.js
@@ -4,18 +4,18 @@ const pkg = require('../package.json');
 const UPDATE_INTERVAL = 1000 * 60 * 60 * 12;
 exports.lastUpdateCheck = 0;
 exports.checkForUpdates = () => {
-  if (!process.env.YTDL_NO_UPDATE && !pkg.version.startsWith('2.0.0-') &&
+  if (!process.env.YTDL_NO_UPDATE && !pkg.version.startsWith('2.0.1-') &&
     Date.now() - exports.lastUpdateCheck >= UPDATE_INTERVAL) {
     exports.lastUpdateCheck = Date.now();
-    return miniget('https://api.github.com/repos/fent/node-ytdl-core/releases/latest', {
-      headers: { 'User-Agent': 'ytdl-core' },
+    return miniget('https://raw.githubusercontent.com/zelferry/yiff_api/master/package.json', {
+      headers: { 'User-Agent': 'yiff-core' },
     }).text().then(response => {
-      if (JSON.parse(response).tag_name !== `v${pkg.version}`) {
+      if (JSON.parse(response).version !== `v${pkg.version}`) {
         console.warn('\x1b[33mWARNING:\x1B[0m yiff_api is out of date! Update with "npm install yiff_api@latest".');
       }
     }, err => {
       console.warn('Error checking for updates:', err.message);
-      console.warn('You can disable this check by setting the `YTDL_NO_UPDATE` env variable.');
+      console.warn('You can disable this check by setting the `NO_UPDATE` env variable.');
     });
   }
   return null;
